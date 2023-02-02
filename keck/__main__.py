79c0afb1d7b03e95c1d5355b412d1ac67e23e297
AartHauk <andy111111111111111@gmail.com>
2023-02-02 11:18:29 -0700

    Moved motor control ui to UI package

--- keck/__main__.py
+++ keck/__main__.py
@@ -3,25 +3,17 @@ import UI # import custom UI elements to main
 import sys
 import stage.motor_ini.core as stg
 
-
-def motor_update (text_area, stage):
-    
-    text_area.config(text=stage.pos)
-
-    text_area.after(1, motor_update, text_area, stage)
-
-def motor_control_frame (parent, stage: int):
-    motor_control = tk.Frame(parent)
-    position = tk.Label(motor_control, text="")
-    position.pack()
-    motor_update(position, stage)
-    return motor_control
-
 motor_id = 0
 def add_motor_new_control(window, stages):
     global motor_id
-    motor_control_frame(window, stages[motor_id]).pack()
+
+    # TODO better stage selection
+    motor_control = UI.motor_controls("TMP", motor_id)
+    motor_control.drawTo(window)
+
     motor_id += 1
+
+    return motor_control
 
 
 def main () -> int:
@@ -30,6 +22,7 @@ def main () -> int:
     window = tk.Tk()
     window.title("Keck - XRay Imaging Sample Control")
     window.geometry("750x270")
+
 
     tk.Button(window, text="Add Motor", command=lambda: add_motor_new_control(window, stages)).pack()
     window.mainloop()
