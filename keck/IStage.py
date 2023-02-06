c741ef6b9b68750a218060395cd2d32a28a0fabd
AartHauk <andy111111111111111@gmail.com>
2023-02-06 16:24:16 -0700

    Basic control and naming complete

--- keck/IStage.py
+++ keck/IStage.py
@@ -7,7 +7,8 @@ class stage:
         self.stage = stage 
         self.id = stage_id
         self.name = f"stage {stage_id}"
-        self.step = 0.000030 # Default 30 nm (smallest possible step size)
+        self.step_size = 0.000030 # Default 30 nm (smallest possible step size)
+        self.acceleration = 0.
         self.platform = sys.platform
 
         if stage == "NOSTAGE":
@@ -16,6 +17,8 @@ class stage:
             self.port = stage.ser_port
 
         print(self.platform)
+
+        self.jog_id = 0
         
 
     @property
@@ -32,4 +35,27 @@ class stage:
         if self.platform == 'linux':
             self.stage.move_home(blocking=True)
         else:
-            print("Not supported")
+            print("Platform not supported")
+
+    def step (self, dist):
+        if self.platform == 'linux':
+            self.stage.move_by(dist, blocking=True)
+        else:
+            print("Platform not supported")
+
+    def start_jog (self, direction, frame):
+        self.velocity = self.step_size * direction
+        self.jog(direction, frame)
+
+    def jog (self, direction, frame):
+        dt = 0.01
+        lim = self.stage.max_vel
+        self.velocity = max(min(self.velocity * direction + self.stage.accn * dt * direction, lim), -lim)  # max of 1 mm/ms
+        if self.platform == 'linux':
+            self.stage.move_by(self.velocity * dt, blocking=True)
+        else:
+            pass
+        self.jog_id = frame.after(1, self.jog, direction, frame)
+
+    def stop_jog (self, frame):
+        frame.after_cancel(self.jog_id)
--- keck/UI.py
+++ keck/UI.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import *
 import stage.motor_ini.core as stg
 import IStage
 
@@ -9,9 +10,38 @@ class motor_controls:
     def motor_position_update (self, text_area):
         text_area.config(text=f'{self.stage.pos:.5f}') # 5 decimal places after mm is 10 nm
         text_area.after(1, self.motor_position_update, text_area)
-    
-    def update (self, parent):
-        pass
+
+    def edit_title (self, parent, label):
+        """
+        ----------------
+        | Change Title |
+        ----------------
+        """
+        edit_window = Toplevel(parent) # Child window 
+        edit_window.geometry("200x200")  # Size of the window 
+        edit_window.title("tmp")
+
+        edit_window.bind('<Key>', lambda event: self.edit_close(event, edit_window))
+
+        edit_window.wm_attributes('-type', 'splash')
+        
+        edit_window.focus_force()
+        edit_window.lift()
+
+        edit_box = tk.Entry(edit_window)
+        edit_box.bind('<Key>', lambda event: self.update_title(edit_box, label))
+        edit_box.insert(0, self.stage.name)
+        edit_box.pack()
+
+        edit_window.mainloop()
+
+    def edit_close (self, event, window):
+        if event.keycode == 36:
+            window.destroy()
+
+    def update_title (self, edit, label):
+        self.stage.name = edit.get()
+        label.config(text = self.stage.name)
 
     def drawTo (self, parent):
         """
@@ -30,6 +60,8 @@ class motor_controls:
         stage_label = tk.Label(motor_control, text=self.stage.name)
         stage_label.grid(row=0, rowspan=1, column=0, columnspan=4)
 
+        stage_label.bind('<ButtonPress-1>', lambda event: self.edit_title(motor_control, stage_label))
+
         stage_remove = tk.Button(motor_control, text="X")
         stage_remove.grid(row=0, rowspan=1, column=4, columnspan=1)
 
@@ -44,17 +76,23 @@ class motor_controls:
         walk_up = tk.Button(motor_control, text="^^")
         walk_up.grid(row=2, rowspan=1, column=0)
 
-        step_up = tk.Button(motor_control, text="^")
+        walk_up.bind('<ButtonPress-1>', lambda event: self.stage.start_jog(1, motor_control))
+        walk_up.bind('<ButtonRelease-1>', lambda event: self.stage.stop_jog(motor_control))
+
+        step_up = tk.Button(motor_control, text="^", command=lambda: self.stage.step(self.stage.step_size))
         step_up.grid(row=2, rowspan=1, column=1)
 
         home = tk.Button(motor_control, text="H", command=self.stage.home)
         home.grid(row=2, rowspan=1, column=2)
 
-        step_down = tk.Button(motor_control, text="V")
+        step_down = tk.Button(motor_control, text="V", command=lambda: self.stage.step(-self.stage.step_size))
         step_down.grid(row=2, rowspan=1, column=3)
 
         walk_down = tk.Button(motor_control, text="VV")
         walk_down.grid(row=2, rowspan=1, column=4)
+
+        walk_down.bind('<ButtonPress-1>', lambda event: self.stage.start_jog(-1, motor_control))
+        walk_down.bind('<ButtonRelease-1>',lambda event: self.stage.stop_jog(motor_control))
 
 if __name__ == '__main__':
     window = tk.Tk()
