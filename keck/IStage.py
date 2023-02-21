ec6caafbec925c8f9ed5e410cb2f0ace6c9b3b44
AartHauk <andy111111111111111@gmail.com>
2023-02-20 19:09:04 -0700

    Add Pos set, change label edit

--- keck/IStage.py
+++ keck/IStage.py
@@ -1,46 +1,55 @@
 import sys
+import tkinter as tk
 import stage.motor_ini.core as stg
 
 # Stage -> stage, name, step, saved_positions[], limits[], inverted, port(?)
 class stage:
     def __init__ (self, stage, stage_id):
-        self.stage = stage 
-        self.id = stage_id
-        self.name = f"stage {stage_id}"
-        self.step_size = 0.000030 # Default 30 nm (smallest possible step size)
-        self.acceleration = 0.
         self.platform = sys.platform
-
         if stage == "NOSTAGE":
             self.platform = "TEST"
         else:
             self.port = stage.ser_port
-
         print(self.platform)
 
+
+        self.stage = stage 
+        self.id = stage_id
+        self.name = tk.StringVar(value=f"stage {stage_id}")
+        self.step_size = 0.000030 # Default 30 nm (smallest possible step size)
+        self.acceleration = 0.
+
+        self.poss = -999
+        self.position = tk.StringVar(value=f'{self.pos:.5f}')
+
         self.jog_id = 0
-        
 
     @property
     def pos (self):
         if self.platform == 'linux':
             return self.stage.pos
         else:
-            return -999
-
-    def set_name (self, name):
-        self.name = name
+            return self.poss
 
     def home (self):
         if self.platform == 'linux':
             self.stage.move_home(blocking=True)
         else:
+            self.poss = 0
+            print("Platform not supported")
+
+    def goto (self, postion):
+        if self.platform == 'linux':
+            self.stage.set_pos(position, blocking=True)
+        else:
+            self.poss = postion
             print("Platform not supported")
 
     def step (self, dist):
         if self.platform == 'linux':
             self.stage.move_by(dist, blocking=True)
         else:
+            self.poss += dist
             print("Platform not supported")
 
     def start_jog (self, direction, frame):
@@ -50,11 +59,12 @@ class stage:
     def jog (self, direction, frame):
         dt = 0.01
         lim = self.stage.max_vel
-        self.velocity = max(min(self.velocity * direction + self.stage.accn * dt * direction, lim), -lim)  # max of 1 mm/ms
+        self.velocity = max(min(self.velocity * direction + self.stage.accn * dt * direction, lim), -lim)  
         if self.platform == 'linux':
             self.stage.move_by(self.velocity * dt, blocking=True)
         else:
-            pass
+            self.poss += self.velocity * dt
+
         self.jog_id = frame.after(1, self.jog, direction, frame)
 
     def stop_jog (self, frame):
--- keck/UI.py
+++ keck/UI.py
@@ -3,6 +3,96 @@ from tkinter import *
 import stage.motor_ini.core as stg
 import IStage
 
+class dropwindow (tk.Toplevel):
+    def __init__(self, parent):
+        tk.Toplevel.__init__(self, parent)
+
+        x = parent.winfo_rootx()
+        y = parent.winfo_rooty()
+        height = parent.winfo_height()
+        geometry = "+%d+%d" % (x, y + height)
+
+        self.geometry(geometry)
+
+        self.wm_attributes('-type', 'splash')
+        self.focus_force()
+        self.lift()
+
+class editable_label (tk.Label):
+    def __init__(self, parent, textvariable):
+        tk.Label.__init__(self, parent, textvariable=textvariable)
+        self.textvariable = textvariable
+        self.bind('<ButtonPress-1>', lambda event: self.edit(self))
+
+    def edit (self, parent):
+        edit_window = dropwindow(parent)
+
+        edit_window.geometry("200x200")  # Size of the window 
+        edit_window.title("label edit")
+
+        edit_window.bind('<Key>', lambda event: self.close(event, edit_window))
+
+        edit_box = tk.Entry(edit_window, textvariable=self.textvariable)
+        edit_box.icursor(len(self.textvariable.get()))
+        edit_box.pack(fill=BOTH)
+
+        edit_box.focus_set()
+
+        edit_window.mainloop()
+
+    def close (self, event, window):
+        if event.keycode == 36:
+            window.destroy()
+
+class position (tk.Canvas):
+    def __init__ (self, parent, stage):
+        tk.Canvas.__init__(self, parent, width= 110, height= 20)
+        self.stage = stage
+
+        self.text = self.create_text(110/2, 20/2 + 4, text=stage.position.get(), fill='black', font=('Helvetica 10'), anchor="center")
+
+        self.bind('<ButtonPress-1>', lambda event: self.setwindow(self))
+        self.after(1, self.motor_position_update)
+
+    def setwindow (self, parent):
+        set_window = dropwindow(parent)
+
+        set_window.geometry("200x200")  # Size of the window 
+        set_window.title("set stage position")
+
+        set_window.bind('<Key>', lambda event: self.check_close(event, set_window, pos))
+
+        pos = StringVar(value=self.stage.position.get())
+
+        edit_box = tk.Entry(set_window, textvariable=pos)
+        edit_box.grid(row=0, rowspan=1, column=0, columnspan=2)
+
+        cancel = tk.Button(set_window, text='Cancel', command=lambda: self.close(set_window))
+        cancel.grid(row=1, rowspan=1, column=0, columnspan=1)
+
+        move = tk.Button(set_window, text='Move', command=lambda: self.gotoPosition(set_window, pos))
+        move.grid(row=1, rowspan=1, column=1, columnspan=1)
+
+        edit_box.focus_set()
+
+        set_window.mainloop()
+
+    def check_close (self, event, window, value):
+        if event.keycode == 36:
+            self.gotoPosition(window, value)
+
+    def gotoPosition (self, window, value):
+        self.stage.goto(float(value.get()))
+        self.close(window)
+
+    def close (self, window):
+        window.destroy()
+
+    def motor_position_update (self):
+        self.stage.position.set(f'{self.stage.pos:.5f}') # 5 decimal places after mm is 10 nm
+        self.itemconfig(self.text, text=self.stage.position.get())
+        self.after(1, self.motor_position_update)
+
 class motor_controls:
     def __init__ (self, stage: IStage.stage):
         self.stage = stage
@@ -10,38 +100,6 @@ class motor_controls:
     def motor_position_update (self, text_area):
         text_area.config(text=f'{self.stage.pos:.5f}') # 5 decimal places after mm is 10 nm
         text_area.after(1, self.motor_position_update, text_area)
-
-    def edit_title (self, parent, label):
-        """
-        ----------------
-        | Change Title |
-        ----------------
-        """
-        edit_window = Toplevel(parent) # Child window 
-        edit_window.geometry("200x200")  # Size of the window 
-        edit_window.title("tmp")
-
-        edit_window.bind('<Key>', lambda event: self.edit_close(event, edit_window))
-
-        edit_window.wm_attributes('-type', 'splash')
-        
-        edit_window.focus_force()
-        edit_window.lift()
-
-        edit_box = tk.Entry(edit_window)
-        edit_box.bind('<Key>', lambda event: self.update_title(edit_box, label))
-        edit_box.insert(0, self.stage.name)
-        edit_box.pack()
-
-        edit_window.mainloop()
-
-    def edit_close (self, event, window):
-        if event.keycode == 36:
-            window.destroy()
-
-    def update_title (self, edit, label):
-        self.stage.name = edit.get()
-        label.config(text = self.stage.name)
 
     def drawTo (self, parent):
         """
@@ -57,18 +115,18 @@ class motor_controls:
         motor_control = tk.Frame(parent)
         motor_control.pack()
 
-        stage_label = tk.Label(motor_control, text=self.stage.name)
+        stage_label = editable_label(motor_control, self.stage.name)
         stage_label.grid(row=0, rowspan=1, column=0, columnspan=4)
-
-        stage_label.bind('<ButtonPress-1>', lambda event: self.edit_title(motor_control, stage_label))
 
         stage_remove = tk.Button(motor_control, text="X")
         stage_remove.grid(row=0, rowspan=1, column=4, columnspan=1)
 
-        stage_position = tk.Label(motor_control, text="00.00000")
+
+        # TODO Test with real motors
+        stage_position = position(motor_control, self.stage) #tk.Label(motor_control, text="00.00000")
         stage_position.grid(row=1, rowspan=1, column=0, columnspan=3)
         
-        self.motor_position_update(stage_position)
+        #self.motor_position_update(stage_position)
 
         units = tk.Label(motor_control, text="mm")
         units.grid(row=1, rowspan=1, column=3, columnspan=2)
